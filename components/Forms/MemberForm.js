8862ad8c4f86e273501763078e4b835bed9c0112
Thomas Rice <ricethomasj@gmail.com>
2023-07-22 13:54:43 -0500

    Added the ability to select a team when making a member

--- components/Forms/MemberForm.js
+++ components/Forms/MemberForm.js
@@ -4,6 +4,7 @@ import { Form, FloatingLabel, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { useAuth } from '../../utils/context/authContext';
 import { createMember, updateMember } from '../../API/membersData';
+import { getTeams } from '../../API/teamData';
 
 const initialState = {
   memberName: '',
@@ -14,11 +15,12 @@ const initialState = {
 
 const MemberForm = ({ obj }) => {
   const [formInput, setFormInput] = useState(initialState);
+  const [teams, setTeams] = useState([]);
   const router = useRouter();
   const { user } = useAuth();
 
   useEffect(() => {
-    // TODO: Add a useState for teams so that I can list them on the form.
+    getTeams(user.uid).then(setTeams);
 
     if (obj.firebaseKey) setFormInput(obj);
     console.warn(obj);
@@ -91,10 +93,34 @@ const MemberForm = ({ obj }) => {
         <Form.Control
           type="text"
           placeholder="Enter Misc Info"
+          aria-label="Enter Misc Info"
           name="wildcard"
           value={formInput.wildcard}
           onChange={handleChange}
         />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingSelect" label="Team">
+        <Form.Select
+          aria-label="Team"
+          name="team"
+          onChange={handleChange}
+          className="mb-3"
+          value={obj.team}
+          required
+        >
+          <option value="">Select a Team</option>
+          {
+            teams.map((item) => (
+              <option
+                key={item.firebaseKey}
+                value={item.firebaseKey}
+              >
+                {item.teamName}
+              </option>
+            ))
+          }
+        </Form.Select>
       </FloatingLabel>
 
       <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Member</Button>
